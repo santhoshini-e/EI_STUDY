38006afafa8ca75561227a42d2eee7b6041bf06f
santhoshini-e <167023908+santhoshini-e@users.noreply.github.com>
2025-10-15 23:20:34 +0530

    refactor : simplify code

--- virtualclassroom/util/Constants.java
+++ virtualclassroom/util/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
                     "Available Commands:\n" +
                     "1. add_classroom <class_name>                    - Create a new classroom\n" +
                     "2. add_student <student_id> <class_name>         - Enroll student in class\n" +
-                    "3. schedule_assignment <class_name> <details>    - Schedule assignment\n" +
-                    "4. submit_assignment <student_id> <class_name> <details> - Submit assignment\n" +
+                    "3. schedule_assignment <class_name> <assignment_id>    - Schedule assignment\n" +
+                    "4. submit_assignment <student_id> <class_name> <assignment_id> - Submit assignment\n" +
                     "5. list_classrooms                               - List all classrooms\n" +
                     "6. list_students <class_name>                    - List students in class\n" +
                     "7. list_assignments <class_name>                 - List assignments for class\n" +
